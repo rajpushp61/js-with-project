ce0a5f528ed662f659ec8884abf26622482c2f8e
rajpushp61 <rajpushp61@gmail.com>
2024-12-24 18:47:27 +0530

    objects

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -1,3 +1,4 @@
 let name = "Pushp"
 let age =18
 let isloggedIn = true
+console.log(name)
---
+++ 01_basics/object.js
@@ -0,0 +1,45 @@
+// const tinderUser = new Object()
+
+const tinderUser = {}
+
+tinderUser.id = "123efc"
+tinderUser.name = "Rahul"
+tinderUser.isloggedIn = false
+
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "abc@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname: "Rahul",
+            lastname:"Jha"
+        }
+    }
+}
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {
+    1:"a",
+    2:"b"
+}
+const obj2 = {
+    3:"c",
+    4:"d"
+}
+// const obj3 = Object.assign({},obj1,obj2)
+
+const obj3 = {...obj1,...obj2}
+
+
+// console.log(obj3);
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('id'));
+
+
+
