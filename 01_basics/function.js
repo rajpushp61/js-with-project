a94f98a9a66d20c92a323d8c4661fde6e260597e
rajpushp61 <rajpushp61@gmail.com>
2024-12-24 19:36:07 +0530

    functions

---
+++ 01_basics/function.js
@@ -0,0 +1,33 @@
+// function name(){
+//     console.log("R");
+//     console.log("A");
+//     console.log("H");
+//     console.log("U");
+//     console.log("L");
+    
+// }
+// name();
+
+// function addTwoNumbers(num1,num2)
+// {
+//     console.log(num1+num2);
+        
+// }
+// const result = addTwoNumbers(2,3);
+
+// function addTwoNumbers(num1,num2)
+// {
+//     let result = num1+num2;
+//     return result;
+        
+// }
+// const result = addTwoNumbers(2,3);
+// console.log(result);
+
+// function logInMessage(name)
+// {
+//     return `${name} just logged In`
+// }
+// // console.log(logInMessage('Rahul'));
+
+// console.log(logInMessage());
