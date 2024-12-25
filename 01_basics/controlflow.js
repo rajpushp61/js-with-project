7558e70ed6ba070e030e34fa784d03ca0e36953e
rajpushp61 <rajpushp61@gmail.com>
2024-12-25 09:54:20 +0530

    controlflow

---
+++ 01_basics/controlflow.js
@@ -0,0 +1,62 @@
+// shorthand notation
+
+// const balance = 1000
+// // if(balance >500)
+// // console.log("test");
+
+// if (balance < 500) {
+//     console.log("less than 500");
+    
+// }
+// else if (balance <700)
+// {
+//     console.log("less than 700");
+    
+// }
+// else{
+//     console.log("less than 1200");
+    
+// }
+
+// const userLoggedIn = true 
+// const debitCard = true
+// const loggedInFromGoogle = false
+// const loggedInFromEmail = false
+
+// if(userLoggedIn &&  debitCard)
+// {
+//     console.log("Allow to buy course");
+    
+// }
+// if (loggedInFromEmail || loggedInFromGoogle) {
+//     console.log("User Logged in");
+     
+// }
+
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month  =  3
+
+// switch (month) {
+//     case 1:
+//         console.log("Jan");
+//     break;
+//     case 2:
+//         console.log("Feb");
+//     break;
+//     case 3:
+//         console.log("March");
+//     break;
+//     default:
+//         console.log("Default matched");
+        
+//         break;
+// }
