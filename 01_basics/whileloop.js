6ee28d1ee090262a3a6b3a29d8ea455b93707c51
rajpushp61 <rajpushp61@gmail.com>
2024-12-25 12:55:39 +0530

    while and do while

---
+++ 01_basics/whileloop.js
@@ -0,0 +1,22 @@
+let index = 0;
+while (index<=10) {
+        console.log(`Value of index is ${index}`);
+        index = index+2
+        
+}
+
+let myarr = ['flash','batman','superman']
+
+let arr = 0
+
+while (arr<myarr.length) {
+    console.log(`Value is ${myarr[arr]}`);
+    arr++
+}
+
+let score = 1
+do {
+    console.log(`${score}`);
+    score++;
+    
+} while (score <= 10);
